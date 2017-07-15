e7187beb322a49c08ed505ca52b71e691ab25a48
MuhBayu <bnugraha00@hotmail.com>
2017-07-15 21:14:44 +0700

    Update nekopoi.php

--- nekopoi.php
+++ nekopoi.php
@@ -3,7 +3,7 @@ header('Content-type: application/json; charset=utf-8;');
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST');
 require_once('src/neko_class.php');
-$_nekopoi = new NekoPoi_();
+$_nekopoi = new PoiPoi\NekoPoi_;
 
 //** Tambahkan parameter ?page= untuk ke halaman berikutnya
 (isset($_REQUEST['page']) && is_numeric($_REQUEST['page'])) ? define('PAGE', $_REQUEST['page']) : define('PAGE', 1);
